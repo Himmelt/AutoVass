669a93e747634317f07f8695597e40971df18137
Himmelt <master@void-3.cn>
2019-06-08 18:32:19 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {title: 'Auto Vass Program'});
+});
+
+router.post('/', function (req, res) {
+    res.render('index', {title: 'Auto Vass Program', data: req.body});
 });
 
 module.exports = router;
