8e92a99dfbdb17588b4e1d12e36134d4a5454131
Himmelt <master@void-3.cn>
2019-06-23 22:44:41 +0800

    update react

--- client/src/SafetyCircle.jsx
+++ client/src/SafetyCircle.jsx
@@ -1,18 +1,53 @@
 import React from 'react';
-import {Button, Icon} from 'antd';
+import {Button, Form, Icon, Tooltip} from 'antd';
 import './SafetyCircle.css';
+import WorkStation from "./WorkStation";
+
+let id = 1;
 
 class SafetyCircle extends React.Component {
+
+    addST = () => {
+        const {form} = this.props;
+        const keys = form.getFieldValue('keys');
+        const nextKeys = keys.concat(id++);
+        form.setFieldsValue({
+            keys: nextKeys,
+        });
+    };
+
     render() {
+        const {getFieldDecorator, getFieldValue} = this.props.form;
+        getFieldDecorator('keys', {initialValue: []});
+        const keys = getFieldValue('keys');
+        const formItems = keys.map((key, index) => (
+            <WorkStation id={key}/>
+        ));
         return (
             <div className='safety-circle'>
-                {this.props.text}
-                <Button type="dashed" style={{width: '100%'}}>
-                    <Icon type="plus"/>添加工作站(ST)
-                </Button>
+                <Tooltip
+                    placement="left"
+                    mouseEnterDelay={0.4}
+                    title="删除安全回路">
+                    <Icon className="minus-circle"
+                          type="minus-circle"
+                          theme="twoTone"
+                          twoToneColor="#ff0000"
+                          onClick={this.props.onRemove}/>
+                </Tooltip>
+                <span>安全回路{this.props.id}</span>
+                {formItems}
+                <Tooltip
+                    placement="top"
+                    mouseEnterDelay={0.4}
+                    title="添加工作站">
+                    <Button type="dashed" onClick={this.addST} style={{width: '100%'}}>
+                        <Icon className="plus-circle" type="plus-circle" theme="twoTone" twoToneColor="#0787f6"/>
+                    </Button>
+                </Tooltip>
             </div>
         );
     }
 }
 
-export default SafetyCircle;
+export default Form.create()(SafetyCircle);
--- client/src/WorkGroup.jsx
+++ client/src/WorkGroup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Form, Icon, Input} from 'antd';
+import {Button, Form, Icon, Input, Tooltip} from 'antd';
 import './WorkGroup.css';
 import SafetyCircle from "./SafetyCircle";
 
@@ -35,17 +35,21 @@ class FormItem extends React.Component {
     }
 }
 
-let id = 0;
+let id = 1;
 
 class WorkGroup extends React.Component {
-
+    remove = k => {
+        const {form} = this.props;
+        const keys = form.getFieldValue('keys');
+        if (keys.length === 0) return;
+        form.setFieldsValue({
+            keys: keys.filter(key => key !== k),
+        });
+    };
     addSK = () => {
         const {form} = this.props;
-        // can use data-binding to get
         const keys = form.getFieldValue('keys');
         const nextKeys = keys.concat(id++);
-        // can use data-binding to set
-        // important! notify form to detect changes
         form.setFieldsValue({
             keys: nextKeys,
         });
@@ -53,35 +57,26 @@ class WorkGroup extends React.Component {
 
     render() {
         const {getFieldDecorator, getFieldValue} = this.props.form;
-        const formItemLayout = {
-            labelCol: {
-                xs: {span: 24},
-                sm: {span: 4},
-            },
-            wrapperCol: {
-                xs: {span: 24},
-                sm: {span: 20},
-            },
-        };
-        const formItemLayoutWithOutLabel = {
-            wrapperCol: {
-                xs: {span: 24, offset: 0},
-                sm: {span: 20, offset: 4},
-            },
-        };
         getFieldDecorator('keys', {initialValue: []});
         const keys = getFieldValue('keys');
         const formItems = keys.map((key, index) => (
-            <SafetyCircle text={key + ',' + index}/>
+            <div key={key}>
+                <SafetyCircle id={key} onRemove={() => this.remove(key)}/>
+            </div>
         ));
         return (
             <div className='work-group'>
                 <Form>
                     <FormItem label='工作组' id='work-group' placeholder='请输入工作组名称'/>
                     {formItems}
-                    <Button type="dashed" onClick={this.addSK} style={{width: '100%'}}>
-                        <Icon type="plus"/>添加安全回路(SK)
-                    </Button>
+                    <Tooltip
+                        placement="top"
+                        mouseEnterDelay={0.4}
+                        title="添加安全回路">
+                        <Button className="add-work-group" type="dashed" onClick={this.addSK} style={{width: '100%'}}>
+                            <Icon className="plus-circle" type="plus-circle" theme="twoTone" twoToneColor="#02cc6b"/>
+                        </Button>
+                    </Tooltip>
                 </Form>
             </div>
         );
