3f10c3227d44d746dcffea7dde626b5114816ba9
Himmelt <master@void-3.cn>
2019-06-24 21:59:20 +0800

    update work-group

--- client/src/WorkGroup.jsx
+++ client/src/WorkGroup.jsx
@@ -10,7 +10,8 @@ class WorkGroup extends React.Component {
         this.state = {
             arg: 'x',
             count: 0,
-            valid: false
+            validateStatus: '',
+            helpText: ''
         };
         this.id = 1;
     }
@@ -30,59 +31,53 @@ class WorkGroup extends React.Component {
     };
 
     add = () => {
-
-        this.props.form.validateFields((err, values) => {
-            console.log('values of form: ', values);
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
-
-        /*        const {form} = this.props;
-                const keys = form.getFieldValue('keys');
-                const nextKeys = keys.concat(this.id++);
-                form.setFieldsValue({
-                    keys: nextKeys,
-                });
-                console.log(nextKeys.length);
-                this.setState({
-                    count: nextKeys.length
-                });*/
+        if (this.state.validateStatus === 'success') {
+            const {form} = this.props;
+            const keys = form.getFieldValue('keys');
+            const nextKeys = keys.concat(this.id++);
+            form.setFieldsValue({
+                keys: nextKeys,
+            });
+            console.log(nextKeys.length);
+            this.setState({
+                count: nextKeys.length
+            });
+        } else if (this.state.validateStatus === '') {
+            this.setState({
+                arg: 'x',
+                validateStatus: 'error',
+                helpText: '必须填写工作组名！'
+            });
+        }
     };
 
     handleChange = (event) => {
         const value = event.target.value;
-        if (value.length === 7) {
+        if (!value) {
             this.setState({
-                arg: value.charAt(6),
-                valid: true
+                arg: 'x',
+                validateStatus: 'error',
+                helpText: '必须填写工作组名！'
+            });
+        } else if (value.length <= 6 || value.length > 7) {
+            this.setState({
+                arg: 'x',
+                validateStatus: 'warning',
+                helpText: '工作组名长度为 7 ，例：K1U1A21'
+            });
+        } else if (!/[A-Z1-9]{6}[1-9]/.test(value)) {
+            this.setState({
+                arg: 'x',
+                validateStatus: 'error',
+                helpText: '请规范填写工作组名，例：K1U1A21'
             });
         } else {
             this.setState({
-                arg: 'x',
-                valid: false
+                validateStatus: 'success',
+                helpText: '',
+                arg: value.charAt(6),
             });
         }
-    };
-
-    validARG = (rule, value, callback) => {
-        if (!value) {
-            callback('')
-        } else if (value.length <= 6) {
-            callback('工作组名长度为 7 ，例：K1U1A21')
-        } else if (!/[A-Z1-9]{6}[1-9]/.test(value)) {
-            callback('请规范填写工作组名，例：K1U1A21');
-        }
-        callback();
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
     };
 
     render() {
@@ -97,20 +92,13 @@ class WorkGroup extends React.Component {
         return (
             <div className='work-group'>
                 <Form>
-                    <Form.Item label='工作组' style={{display: 'flex'}}>
-                        {getFieldDecorator('work-group', {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: '必须填写工作组名称！',
-                                },
-                                {
-                                    validator: this.validARG
-                                }
-                            ],
-                        })(<Input disabled={this.state.count !== 0}
-                                  autoComplete="off" placeholder='请输入工作组名称'
-                                  onChange={this.handleChange}/>)}
+                    <Form.Item hasFeedback
+                               validateStatus={this.state.validateStatus}
+                               help={this.state.helpText}
+                               label='工作组' style={{display: 'flex'}}>
+                        <Input style={{minWidth: '16em', color: 'black'}} disabled={this.state.count !== 0}
+                               autoComplete="off" placeholder='请输入工作组名称'
+                               onChange={this.handleChange}/>
                     </Form.Item>
                     {formItems}
                     <Tooltip
